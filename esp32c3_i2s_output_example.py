d3045bffe0b351c37d01dcf771484dc67af0c9b1
charkster <31560568+charkster@users.noreply.github.com>
2025-09-17 21:50:45 -0700

    Add files via upload

---
+++ esp32c3_i2s_output_example.py
@@ -0,0 +1,43 @@
+from machine import I2S
+from machine import Pin
+import ustruct
+
+# these pins 
+def init_i2s(rate=44100, bits=8, format=I2S.MONO):
+    return I2S(
+        0,
+        sck    = Pin(8),  # BCLK
+        ws     = Pin(9),  # LRC
+        sd     = Pin(10), # DIN
+        mode   = I2S.TX,
+        bits   = bits,
+        format = format,
+        rate   = rate,
+        ibuf   = 2048 )
+
+def read_wav_header(file):
+    header = file.read(44)
+    riff, size, wave = ustruct.unpack('<4sI4s', header[0:12])
+    if riff != b'RIFF' or wave != b'WAVE':
+        raise ValueError("Not a valid WAV file")
+    
+    audio_format, num_channels, sample_rate = ustruct.unpack('<HHI', header[20:28])
+    byte_rate, block_align, bits_per_sample = ustruct.unpack('<IHH', header[28:36])
+    
+    if (num_channels == 1):
+        i2s = init_i2s(rate=sample_rate, bits=bits_per_sample, format=I2S.MONO)
+    else:
+        i2s = init_i2s(rate=sample_rate, bits=bits_per_sample, format=I2S.STEREO)
+    return i2s
+
+def play_wav(filename):
+    with open(filename, "rb") as f:
+#        file.read(44) # skip header
+        i2s = read_wav_header(f)
+        while True:
+            data = f.read(1024)
+            if not data:
+                break
+            i2s.write(data)
+
+play_wav("music-16k-16bits-mono.wav")
